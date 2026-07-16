80bd4104788c2992a2157e1005a99a95a8521a07
Zineb <m.zineb@Zinebs-MacBook-Pro.local>
2026-07-16 20:54:34 +0200

    feat: create phone style

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php echo "Hallo, het werkt!"; ?>
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Virtual Currency</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="phone"></div>
+</body>
+</html>
